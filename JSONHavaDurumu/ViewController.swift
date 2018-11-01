311a78c95e49e4ef349b12c87aa07aedf43be91e
Gokhan Gokova <gokhan@gokhangokova.com>
2018-11-01 21:54:14 +0300

    Initial Commit

---
+++ JSONHavaDurumu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONHavaDurumu
+//
+//  Created by Gokhan Gokova on 1.11.2018.
+//  Copyright © 2018 Gokhan Gokova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
