164da4f6ecf8bb827018b2ee37e322cc52f53e2c
Gokhan Gokova <gokhan@gokhangokova.com>
2018-11-02 10:48:29 +0300

    JSON Kullanımı ile ilgili; button ile URLSession, viewDidLoad içerisinde Swift 4 Codable ile JSON hava durumu uygulaması

--- JSONHavaDurumu/ViewController.swift
+++ JSONHavaDurumu/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ilText: UITextField!
+    @IBOutlet weak var ilLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&lang=tr&appid=9b6db371af398e1e94d77d7012f4913c&units=metric"
+        if let url = URL(string: url) {
+            let request =  URLRequest(url: url)
+            
+            URLSession.shared.dataTask(with: request) { (data, response, err) in
+                let Json = JSONDecoder()
+                do {
+                    let newJson = try Json.decode(Weather.self, from: data!)
+                    print(newJson.main.temp)
+                } catch {}
+            }.resume()
+        }
     }
+    
+    
+    @IBAction func getirClicked(_ sender: Any) {
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=\(ilText.text!)&lang=tr&appid=9b6db371af398e1e94d77d7012f4913c&units=metric"
+        
+        if let url = (URL(string: url)) {
+            let request = URLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                if error == nil {
+                    if let data = data {
+                        do {
+                            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                            
+                            let weather = jsonResult["weather"] as! NSArray
+                            let desc = weather.firstObject as! [String : AnyObject]
+                            
+                            if let weatherDescription = desc["description"] as? String {
+                                DispatchQueue.main.sync {
+                                    self.ilLabel.text = "\(self.ilText.text!) için hava şu anda \(weatherDescription)"
+                                }
+                            }
+                            
+                            print(jsonResult)
+                            
+                            
+                        }catch {
+                            
+                        }
+                    }
+                    
+                }
+            }
+            task.resume()
+        }
 
-
+    }
+    
 }
-
---
+++ JSONHavaDurumu/Weather.swift
@@ -0,0 +1,40 @@
+//
+//  Weather.swift
+//  JSONHavaDurumu
+//
+//  Created by Gokhan Gokova on 2.11.2018.
+//  Copyright © 2018 Gokhan Gokova. All rights reserved.
+//
+
+// To parse the JSON, add this file to your project and do:
+//
+//   let weather = try? newJSONDecoder().decode(Weather.self, from: jsonData)
+
+import Foundation
+
+struct Weather: Codable {
+    let weather: [WeatherElement]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let dt: Int
+    let id: Int
+    let name: String
+    let cod: Int
+}
+
+struct Main: Codable {
+    let temp: Double
+    let pressure, humidity, tempMin, tempMax: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case temp, pressure, humidity
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+}
+
+struct WeatherElement: Codable {
+    let id: Int
+    let main, description, icon: String
+}
